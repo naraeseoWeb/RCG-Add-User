6d8dc29519e860a2c557fd7da6c013074d810039
naraeseo <naraeseoez@gmail.com>
2022-09-16 13:25:38 +0900

    instructor version init

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
-import AddUserInput from './Components/AddUserInput';
 import './App.css';
-import { useState } from 'react';
-import AddUserList from './Components/AddUserList';
 
 function App() {
-  const [userInfo, setUserInfo] = useState([]);
-  const [addedUser, setAddedUser] = useState(false);
-
-  const AddUserHandler = (enteredText) => {
-    setUserInfo((prevText) => {
-      const updatedText = [...prevText];
-      updatedText.push({
-        name: enteredText.name,
-        age: enteredText.age,
-        id: Math.random().toString(),
-      });
-      setAddedUser(true);
-      return updatedText;
-    });
-  };
-
-  return (
-    <>
-      <div className='App'>
-        <section className='AddUser-form'>
-          <AddUserInput onAddUser={AddUserHandler} />
-        </section>
-        <section className='User-List'>
-          <AddUserList onAddUser={addedUser} items={userInfo} />
-        </section>
-      </div>
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
